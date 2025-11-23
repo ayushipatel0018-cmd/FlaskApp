f6584b18bc72a52867e0ae6cfccff81af00f3b13
ayushi <ayushipatel0018@gmail.com>
2025-11-23 20:23:25 +0530

    Full CI/CD workflow fixed

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,7 @@
 from app import app
 
 def test_home():
-    test_client = app.test_client()
-    response = test_client.get("/")
+    client = app.test_client()
+    response = client.get("/")
     assert response.status_code == 200
     assert response.data == b"Hello, World!"
